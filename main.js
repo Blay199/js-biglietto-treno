bc10804e6a23f1f15ed9fbd1074e2c392fc7c3a7
Blay199 <a.sorrentino03@gmail.com>
2023-02-21 17:52:10 +0100

    Prova inizializzazione

---
+++ main.js
@@ -0,0 +1,16 @@
+const priceKm = 0.21
+
+let userKm = parseInt(prompt('Inserisci Km da percorrere'))
+let userAge = parseInt(prompt('Inserire età passeggero'))
+
+let ticketPrice = userKm * priceKm
+
+if (userAge < 18) {
+    ticketPrice * 0.80
+} else if (userAge > 65) {
+    ticketPrice * 0.60
+}
+
+let resultStamp = document.getElementById('ticketPrice')
+resultStamp.innerHTML = 'Il prezzo del biglietto per la tua fascia di età è: ' + ticketPrice.toFixed(2)
+console.log(userAge)
